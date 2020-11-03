97970d875cef1e78ad29e7c72ea1df00e6191672
HbC <hbcsoft@outlook.com>
2020-11-03 22:35:38 +0000

    Update to sample project

---
+++ samples/WpfExcelSample/UserControl1.xaml.vb
@@ -0,0 +1,18 @@
+﻿Imports System.Data
+
+Public Class UserControl1
+
+    Private Sub Button_Click(sender As Object, e As System.Windows.RoutedEventArgs)
+
+        'Build a data table
+        Dim dt As New DataTable
+        dt.Columns.Add("ID")
+        dt.Columns.Add("NAME")
+        dt.Columns.Add("DATE")
+
+        dt.Rows.Add({1, "Jonh", New Date(1981, 10, 12)})
+        dt.Rows.Add({1, "Ford", New Date(1971, 1, 22)})
+
+    End Sub
+
+End Class
